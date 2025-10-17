f0984561597728cb2ec7759abd9fb10ad8ed7497
Josace <josace@MacBook-Air-de-Josace.local>
2025-10-16 21:12:37 -0500

    tercer commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo senior!");
+        System.out.println("Hola mundo senior!");
     }
 }
 |
